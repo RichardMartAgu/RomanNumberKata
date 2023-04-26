080772091729624ec839878293fe03139d2a59ee
Richard <125735052+RichardMartAgu@users.noreply.github.com>
2023-04-26 22:05:07 +0200

    Refactor de variables al ingles

--- src/main/java/org/sValero/RomanNumberConvert.java
+++ src/main/java/org/sValero/RomanNumberConvert.java
@@ -1,22 +1,22 @@
 package org.sValero;
 
 public class RomanNumberConvert {
-    private static final String[] UNIDADES = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
-    private static final String[] DECENAS = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
-    private static final String[] CENTENAS = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
-    private static final String[] MILES = {"", "M", "MM", "MMM"};
+    private static final String[] UNITS = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+    private static final String[] TENS = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+    private static final String[] HUNDREDS = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+    private static final String[] THOUSANDS = {"", "M", "MM", "MMM"};
 
     public static String convert(int number) {
         if (number < 1 || number > 3000) {
             throw new IllegalArgumentException("El número debe estar entre 1 y 1000");
         }
 
-        int unidad = number % 10;
-        int decena = (number / 10) % 10;
-        int centena = (number / 100) % 10;
-        int mil = number / 1000;
+        int unit  = number % 10;
+        int ten  = (number / 10) % 10;
+        int hundred  = (number / 100) % 10;
+        int thousand  = number / 1000;
 
-        return MILES[mil] + CENTENAS[centena] + DECENAS[decena] + UNIDADES[unidad];
+        return THOUSANDS[thousand ] + HUNDREDS[hundred ] + TENS[ten ] + UNITS[unit ];
     }
 
 }
