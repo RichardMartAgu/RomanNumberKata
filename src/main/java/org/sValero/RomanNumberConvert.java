8168517d19c84e59329717f308abbfb2b6fa1bc5
Richard <125735052+RichardMartAgu@users.noreply.github.com>
2023-04-28 23:16:21 +0200

    Ajustando a pruebas de lintr

--- src/main/java/org/sValero/RomanNumberConvert.java
+++ src/main/java/org/sValero/RomanNumberConvert.java
@@ -5,10 +5,25 @@ package org.sValero;
     **/
 public final class RomanNumberConvert {
 
+    /**
+     * Número mínimo
+     */
     private static final int MINIMUM_NUMBER = 1;
+    /**
+     * Número máximo
+     */
     private static final int MAXIMUM_NUMBER = 3000;
+    /**
+     * Número fijo para cálculos
+     */
     private static final int UNITS_IN_TEN = 10;
+    /**
+     * Número fijo para cálculos
+     */
     private static final int UNITS_IN_HUNDRED = 100;
+    /**
+     * Número fijo para cálculos
+     */
     private static final int UNITS_IN_THOUSAND = 1000;
 
     /**
@@ -21,21 +36,21 @@ public final class RomanNumberConvert {
      * Representación de las decenas en números romanos.
      */
 
-    private static final String[] TENS = {"", "X", "XX", "XXX", "XL",
+    private static final String[] TENS={"", "X", "XX", "XXX", "XL",
             "L", "LX", "LXX", "LXXX", "XC"};
 
     /**
      * Representación de las centenas en números romanos.
      */
 
-    private static final String[] HUNDREDS = {"", "C", "CC", "CCC", "CD",
+    private static final String[] HUNDREDS={"", "C", "CC", "CCC", "CD",
             "D", "DC", "DCC", "DCCC", "CM"};
 
     /**
      * Representación de los millares en números romanos.
      */
 
-    private static final String[] THOUSANDS ={"", "M", "MM", "MMM"};
+    private static final String[] THOUSANDS={"", "M", "MM", "MMM"};
 
     //Constructor privado para evitar la creación de instancias de la clase.
 
@@ -43,18 +58,18 @@ public final class RomanNumberConvert {
 
     //Convierte un número entero en su representación numérica romana.
 
-    public static String convert(final int number) {
+    public static String convert(final int number){
 
 
 
-        if (number < MINIMUM_NUMBER || number > MAXIMUM_NUMBER) {
+        if (number < MINIMUM_NUMBER || number > MAXIMUM_NUMBER){
             throw new IllegalArgumentException("El número debe estar entre 1 y 3000");
         }
 
-        final int unit = number % UNITS_IN_TEN;
-        final int ten = (number / UNITS_IN_TEN) % UNITS_IN_TEN;
-        final int hundred = (number / UNITS_IN_HUNDRED) % UNITS_IN_TEN;
-        final int thousand = number / UNITS_IN_THOUSAND;
+        final int unit=number % UNITS_IN_TEN;
+        final int ten=(number / UNITS_IN_TEN) % UNITS_IN_TEN;
+        final int hundred=(number / UNITS_IN_HUNDRED) % UNITS_IN_TEN;
+        final int thousand=number / UNITS_IN_THOUSAND;
 
         return THOUSANDS[thousand] + HUNDREDS[hundred] + TENS[ten] + UNITS[unit];
     }
