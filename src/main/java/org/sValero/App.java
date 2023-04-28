95bafb6f4534727f65b3e97e4c0a912877395d14
Richard <125735052+RichardMartAgu@users.noreply.github.com>
2023-04-28 22:37:02 +0200

    Ajustando a pruebas de lintr

--- src/main/java/org/sValero/App.java
+++ src/main/java/org/sValero/App.java
@@ -1,10 +1,17 @@
 package org.sValero;
 
-public class App {
+public final class App {
     /**
-     * Método main
+     * Método principal de la aplicación.
+     *
+     * @param args los argumentos de la línea de comandos.
      */
     public static void main(final String[] args) {
         // Código aquí
+    }
+
+    // Evita que se cree una instancia de la clase App.
+    private App() {
+        throw new AssertionError("No se debe instanciar la clase App.");
     }
 }
